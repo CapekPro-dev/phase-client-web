0cb6d75304391b0d744b948419879deb0540d21a
CapekPro-dev <58908429+CapekPro-dev@users.noreply.github.com>
2021-09-03 18:08:33 +0200

    Update route.php

--- app/route.php
+++ app/route.php
@@ -5,30 +5,30 @@
    $get_requested_uri = $split_requested_uri[1];
    
    if($get_requested_uri == null || $get_requested_uri == ''){
-	   require_once '../public/home.php';
+	   require_once './home.php';
    }
    else{
 	   switch ($get_requested_uri){
 		   case 'home':
-		      require_once '../public/home.php';
+		      require_once './home.php';
 			  break;
 			  
 		   case 'support':
-		      require_once '../public/support.php';
+		      require_once './support.php';
 			  break;
 			  
 		   case 'store':
-		      require_once '../public/store.php';
+		      require_once './store.php';
 			  break;
 		   case 'download':
-			  require_once '../public/download.php';
+			  require_once './download.php';
 			  break;
 		   case 'upload':
-			  require_once '../public/index.php';
+			  require_once './index.php';
 			  break;
 			  
 		   default:
-		      require_once '../public/404.php';
+		      require_once './404.php';
 			  break;
 	   }
    }
