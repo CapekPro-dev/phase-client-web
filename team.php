cce7dacefcd5cdb2dc7bc2b50cf9656cb0cb5bd8
CapekPro-dev <58908429+CapekPro-dev@users.noreply.github.com>
2021-09-03 18:12:07 +0200

    Update team.php

--- team.php
+++ team.php
@@ -5,15 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team | Phase Client</title>
-    <link rel="shortcut icon" href="../assest/media/img/logo.png">
-    <link rel="stylesheet" href="../assest/css/style.css">
+    <link rel="shortcut icon" href="./assest/media/img/logo.png">
+    <link rel="stylesheet" href="./assest/css/style.css">
 
 
 </head>
 <body>
     <div class="head-bg">
         <div style="background: none;">
-    <img src="../assest/media/img/phase-client.png" class="logo">
+    <img src="./assest/media/img/phase-client.png" class="logo">
     </div>
     <nav class="navbar">
         <ul>
@@ -27,31 +27,31 @@
     <div class="t-box">
         <div class="t-owner">
             <h1>OWNER</h1>
-            <h2 class="ownerv"><img src="../assest/media/img/vojtas.png" class="vojtas"><br> <b>VojtasCZ</b></h2>
+            <h2 class="ownerv"><img src="./assest/media/img/vojtas.png" class="vojtas"><br> <b>VojtasCZ</b></h2>
         </div>
         <div class="t-coowner">
         <h1>CO-OWNER</h1>
-        <h2 class="ownerv"><img src="../assest/media/img/capekpro.png" class="vojtas"><br> <b1>CapekPro</b1></h2>
+        <h2 class="ownerv"><img src="./assest/media/img/capekpro.png" class="vojtas"><br> <b1>CapekPro</b1></h2>
         </div>
         <div class="t-admin">
         <h1>ADMIN</h1>
-        <h2 class="ownerv"><img src="../assest/media/img/cirax.png" class="vojtas"><br> <b2>Cirax856</b2></h2>
+        <h2 class="ownerv"><img src="./assest/media/img/cirax.png" class="vojtas"><br> <b2>Cirax856</b2></h2>
         </div>
         <div class="t-dev">
         <h1>DEVELOPER</h1>
         </div>
         <div class="t-staff">
         <h1>STAFF</h1>
-        <h2 class="ownerv"><img src="../assest/media/img/nexus.jpg" class="vojtas"><br> <b3>NexusSK</b3></h2>
+        <h2 class="ownerv"><img src="./assest/media/img/nexus.jpg" class="vojtas"><br> <b3>NexusSK</b3></h2>
         </div>
         <div class="t-gdteam">
         <h1>G & D TEAM</h1>
-        <h2 class="ownerv"><img src="../assest/media/img/reggye.png" class="vojtas"><br> <b4>Itz_Reggye</b4></h2>
+        <h2 class="ownerv"><img src="./assest/media/img/reggye.png" class="vojtas"><br> <b4>Itz_Reggye</b4></h2>
         </div>
     </div>
     <div class="footer">
         <br>
-        <img src="../assest/media/img/phaseclient.png">
+        <img src="./assest/media/img/phaseclient.png">
         <p class="foot"><br><br>&copy Phase Client 2021</p>
         <p class="foot2">Not affilated with Mojang AB</p>
         <p class="foot3"><a href="/support">Support</a></p>
