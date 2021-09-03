9040ef720de608f3e485e0b84ab0ea518896fafb
CapekPro-dev <58908429+CapekPro-dev@users.noreply.github.com>
2021-09-03 18:10:26 +0200

    Update download.php

--- download.php
+++ download.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Download | Phase Client</title>
-    <link rel="shortcut icon" href="../assest/media/img/logo.png">
-    <link rel="stylesheet" href="../assest/css/style.css">
+    <link rel="shortcut icon" href="./assest/media/img/logo.png">
+    <link rel="stylesheet" href="./assest/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 
 
@@ -14,7 +14,7 @@
 <body>
     <div class="head-bg">
         <div style="background: none;">
-    <img src="../assest/media/img/phase-client.png" class="logo">
+    <img src="./assest/media/img/phase-client.png" class="logo">
     </div>
     <nav class="navbar">
         <ul>
@@ -26,16 +26,16 @@
         </ul>
     </nav>
     <div class="win-download">
-    <img src="../assest/media/img/windows-brands.svg" class="win"><br>
-    <img src="../assest/media/img/win.png" class="win2">
+    <img src="./assest/media/img/windows-brands.svg" class="win"><br>
+    <img src="./assest/media/img/win.png" class="win2">
     </div>
     <div class="mac-download">
-    <img src="../assest/media/img/apple-brands.svg" class="win"><br>
-    <img src="../assest/media/img/macos.png" class="win2">
+    <img src="./assest/media/img/apple-brands.svg" class="win"><br>
+    <img src="./assest/media/img/macos.png" class="win2">
     </div>
     <div class="linux-download">
-    <img src="../assest/media/img/linux-brands.svg" class="win"><br>
-    <img src="../assest/media/img/linux.png" class="win2">
+    <img src="./assest/media/img/linux-brands.svg" class="win"><br>
+    <img src="./assest/media/img/linux.png" class="win2">
     </div>
     <button class="btn1">SOON</button>
     <button class="btn2">SOON</button>
@@ -43,7 +43,7 @@
 
     <div class="footer">
         <br>
-        <img src="../assest/media/img/phaseclient.png">
+        <img src="./assest/media/img/phaseclient.png">
         <p class="foot"><br><br>&copy Phase Client 2021</p>
         <p class="foot2">Not affilated with Mojang AB</p>
         <p class="foot3"><a href="/support">Support</a></p>
